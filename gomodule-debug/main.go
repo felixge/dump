d5c0250549eaa6af91c782b2dcac816b0096f508
Felix Geisendörfer <felix@debuggable.com>
2020-10-14 12:02:36 +0200

    simplify

--- gomodule-debug/main.go
+++ gomodule-debug/main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	_ "github.com/prometheus/client_golang/prometheus"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-}
-
-func run() error {
-	return nil
+	fmt.Printf("hello world\n")
 }
