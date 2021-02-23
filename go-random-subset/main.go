e88851d6c9796cf3442d3d38190e6a19bf127387
Felix Geisendörfer <felix@felixge.de>
2021-02-23 21:21:15 +0100

    add url

--- go-random-subset/main.go
+++ go-random-subset/main.go
@@ -22,16 +22,26 @@ var algorithms = []struct {
 	Name string
 	Fn   func(k int, in []int) []int
 }{
+	{"fromOffset", fromOffset},
 	{"fisherYatesSubset", fisherYatesSubset},
+	{"hashDedupe", hashDedupe},
+	//{"hashDedupeFlip", hashDedupeFlip},
+	{"hybridHashDedupeFisherYates", hybridHashDedupeFisherYates},
 	//{"richardExp", richardExp},
 	{"algorithmL", algorithmL},
 }
 
 func fisherYatesSubset(k int, in []int) []int {
-	rand.Shuffle(len(in), func(i, j int) {
-		in[i], in[j] = in[j], in[i]
+	if k >= len(in) {
+		return in
+	}
+
+	inCopy := make([]int, len(in))
+	copy(inCopy, in)
+	rand.Shuffle(len(inCopy), func(i, j int) {
+		inCopy[i], inCopy[j] = inCopy[j], inCopy[i]
 	})
-	return in[0:k]
+	return inCopy[0:k]
 }
 
 func richardExp(k int, in []int) []int {
@@ -51,8 +61,78 @@ func richardExp(k int, in []int) []int {
 	return sample
 }
 
+func hashDedupeFlip(k int, in []int) []int {
+	if k >= len(in) {
+		return in
+	}
+
+	m := map[int]struct{}{}
+	out := make([]int, 0, k)
+	flip := k > len(in)/2
+
+	for len(out) < k {
+		i := rand.Intn(len(in))
+		if _, dupe := m[i]; dupe {
+			continue
+		}
+		if !flip {
+			out = append(out, in[i])
+		}
+	}
+	//if flip {
+	//for i, v := range in {
+	//if _, ok
+	//out = append(out, v)
+	//}
+	//}
+	return out
+}
+
+func hashDedupe(k int, in []int) []int {
+	if k >= len(in) {
+		return in
+	}
+
+	picked := map[int]struct{}{}
+	out := make([]int, 0, k)
+	for len(out) < k {
+		i := rand.Intn(len(in))
+		if _, dupe := picked[i]; dupe {
+			continue
+		}
+		picked[i] = struct{}{}
+		out = append(out, in[i])
+	}
+	return out
+}
+
+func fromOffset(k int, in []int) []int {
+	if k >= len(in) {
+		return in
+	}
+	out := make([]int, 0, k)
+	offset := rand.Intn(len(in))
+	for i := 0; i < k; i++ {
+		out = append(out, in[(i+offset)%len(in)])
+	}
+	return out
+}
+
+// https://www.codementor.io/@alexanderswilliams/how-to-efficiently-generate-a-random-subset-150hbz3na4
+func hybridHashDedupeFisherYates(k int, in []int) []int {
+	if float64(k) < float64(0.63212055882)*float64(len(in)) {
+		return hashDedupe(k, in)
+	} else {
+		return fisherYatesSubset(k, in)
+	}
+}
+
 // https://en.wikipedia.org/wiki/Reservoir_sampling#An_optimal_algorithm
 func algorithmL(k int, s []int) []int {
+	if k >= len(s) {
+		return s
+	}
+
 	n := len(s)
 	r := make([]int, k)
 	i := 0
--- go-random-subset/main_test.go
+++ go-random-subset/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 	"testing"
@@ -16,37 +17,64 @@ func TestCorrectness(t *testing.T) {
 	n := 1000
 	tolerance := 0.01
 
-	original := makeSlice(n)
-	originalMean := mean(original)
+	in := makeSlice(n)
+	inMean := mean(in)
 
 	for _, a := range algorithms {
 		t.Run(a.Name, func(t *testing.T) {
-			for k := 1; k < n; k += n / 100 {
-				in := make([]int, len(original))
-				copy(in, original)
-
+			for k := 1; k < n*2; k += n / 100 {
 				var (
-					sampleMeanSum float64
-					sampleCount   int
-					minErr        float64
-					iterations    = n * 10
+					outMeanSum float64
+					minErr     float64
+					iterations = n * 10
 				)
 				for i := 0; i < iterations; i++ {
-					sampleMeanSum += mean(a.Fn(k, in))
-					sampleCount++
-					samplesMean := sampleMeanSum / float64(sampleCount)
-					samplesMeanError := math.Abs(originalMean-samplesMean) / originalMean
-					if samplesMeanError < tolerance {
+					dupes := map[int]struct{}{}
+					out := a.Fn(k, in)
+					for _, v := range out {
+						if _, dupe := dupes[v]; dupe {
+							t.Fatal("dupe detected")
+						}
+						dupes[v] = struct{}{}
+					}
+
+					if (k <= n && len(out) != k) || (k > n && len(out) != n) {
+						t.Fatal("bad output length")
+					}
+
+					outMeanSum += mean(out)
+					outMean := outMeanSum / float64(i+1)
+					outMeanError := math.Abs(inMean-outMean) / inMean
+					if outMeanError < tolerance {
 						//t.Logf("k=%d i=%d", k, i)
 						break
 					} else if i+1 == iterations {
 						t.Fatalf("sample mean did not converge on population mean: k=%d n=%d iterations=%d minErr=%f", k, n, iterations, minErr)
-					} else if samplesMeanError < minErr || minErr == 0 {
-						minErr = samplesMeanError
+					} else if outMeanError < minErr || minErr == 0 {
+						minErr = outMeanError
 					}
 				}
 			}
 		})
+	}
+}
+
+func BenchmarkAlgorithms(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+
+	n := 100000
+	for _, a := range algorithms {
+		for p := float64(1); p < 100; p += 9.8 {
+			k := int(float64(n) * (p / 100))
+
+			name := fmt.Sprintf("%s-%d", a.Name, int(math.Round(p)))
+			b.Run(name, func(b *testing.B) {
+				input := makeSlice(n)
+				for i := 0; i < b.N; i++ {
+					a.Fn(k, input)
+				}
+			})
+		}
 	}
 }
 
